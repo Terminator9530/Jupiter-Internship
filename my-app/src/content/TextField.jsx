20278b57388b5ce11efdebd7f96896ce9c27349a
VAIBHAV SHUKLA <shuklavaibhav0306@gmail.com>
2020-05-07 11:35:13 +0530

    updated

--- my-app/src/content/TextField.jsx
+++ my-app/src/content/TextField.jsx
@@ -1,19 +1,35 @@
 import React from 'react';
 function TextField(props) {
-  console.log(props.err);
-  const [val, setVal] = React.useState("");
-
-  function changeText(event) {
-    var inputVal = event.target.value.trimLeft().replace(/\s\s+/g, ' ');
-    // if (props.setFn) // if the function is set
-    //   props.setFn(inputVal);
-    setVal(inputVal);
-  }
+  const [val, setVal] = React.useState({err:"",val:""});
+  function validate(txt){
+        var pattern=/^[a-zA-Z ]*$/;
+        return pattern.test(txt);
+      }
+  
+      function changeText(event){
+        
+        var inputVal = event.target.value.trimLeft().replace(/\s\s+/g, ' ');
+        console.log("input",inputVal);
+        if(inputVal!==""){         
+            if(inputVal.length>30){
+              return setVal({...val,err:"Length Should not be greater than 30",val:inputVal});
+            } else {
+              if(!validate(inputVal)){
+                console.log("no number");
+                return setVal({...val,err:"Contains only text",val:inputVal});
+              }
+            }
+        } else {
+          return setVal({...val,err:"Should Not Be Empty",val:inputVal});
+        }
+        return setVal({...val,err:"",val:inputVal});
+    }
   const a = "textField " + props.class + " form-control";
+  console.log("val",val);
   return (
     <span className="form-group">
-      <input id={props.id} type="text" value={val} onChange={changeText} className={a} autoComplete="off" />
-      <small className="form-text" style={{ "color": "red" }}>{props.err}</small>
+      <input id={props.id} type="text" value={val.val} onChange={changeText} className={a} autoComplete="off" />
+      <small className="form-text" style={{ "color": "red" }}>{props.err?props.err:val.err}</small>
     </span>
   );
 }
