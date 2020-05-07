cf75ddfd9c148d0a8231a2cd22c6e4a587ba3ed7
VAIBHAV SHUKLA <shuklavaibhav0306@gmail.com>
2020-05-07 11:14:10 +0530

    updated

--- my-app/src/content/Profile.jsx
+++ my-app/src/content/Profile.jsx
@@ -5,7 +5,29 @@ import DatePicker from "./DatePicker";
 import Zoom from '@material-ui/core/Zoom';
 import TextField from './TextField';
 
+
 function Profile(){
+    const [err,setErr]=React.useState({name:"",prof:"",desg:""});
+    //const [stateName,setName] = React.useState("");    
+    function validate(txt){
+        var pattern=/^[a-zA-Z ]*$/;
+        return pattern.test(txt);
+      }
+  
+      function changeText(txt,key){
+        if(txt!==""){         
+            if(txt.length>30){
+              return "Length Should not be greater than 30";
+            } else {
+              if(!validate(txt)){
+                return "Contains only text";
+              }
+            }
+        } else {
+          return "Should Not Be Empty";
+        }
+        return "";
+    }
     function submit(e){
         e.preventDefault();
         console.log("clicked");
@@ -13,21 +35,33 @@ function Profile(){
         var DOB=document.getElementById("DOB").value;
         var prof=document.getElementById("Profession").value;
         var desg=document.getElementById("Designation").value;
-        console.log(name,DOB,prof,desg);
+        //console.log(name,DOB,prof,desg);
+        var nameErr = changeText(name,"name");        
+        var desgErr = changeText(desg,"desg");
+        var profErr = changeText(prof,"prof");
+        console.log(nameErr,desgErr,profErr);
+        if((nameErr=="")&&(desgErr=="")&&(profErr=="")){
+            console.log("Valid");
+            // put your submit code here
+        } else {
+            console.log("Invalid");
+        }
+        setErr({...err,name:nameErr,desg:desgErr,prof:profErr});
     }
-    return(
+    //console.log("state changed", JSON.stringify(err));
+   return(
         <Zoom in={true}>
         <div style={{'margin':'50px auto','width':'60%','height':'auto'}}>
             <center><Label text="Profile Info" style={{'margin':'0'}} />
             <form onSubmit={submit} style={{'padding':'10px','height':'auto','display':'block','margin':'50px 0','border':'1px solid black'}} className="row polaroid">
                 <Label class="col-xl-2 col-md-12 col-12" text="Name" />
-                <TextField id="Name" class="col-xl-8 col-md-12 col-12" /><br />
+                <TextField err={err.name} id="Name" class="col-xl-8 col-md-12 col-12" /><br />
                 <Label class="col-xl-2 col-md-12 col-12" text="DOB" />
                 <DatePicker id="DOB" class="col-xl-8 col-md-12 col-12" /><br />
                 <Label class="col-xl-2 col-md-12 col-12" text="Profession" />
-                <TextField id="Profession" class="col-xl-8 col-md-12 col-12" /><br />
+                <TextField err={err.prof} id="Profession" class="col-xl-8 col-md-12 col-12" /><br />
                 <Label class="col-xl-2 col-md-12 col-12" text="Designation" />
-                <TextField id="Designation" class="col-xl-8 col-md-12 col-12" /><br />
+                <TextField err={err.desg} id="Designation" class="col-xl-8 col-md-12 col-12" /><br />
                 <Button text="Cancel" color="secondary" style={{'margin':'30px 20px 0px 0px'}} /> 
                 <Button id="set_submit" text="Submit" style={{'backgroundColor':'#47c20a','color':'white','margin':'30px 0px 0px 20px'}} />
             </form></center>
--- my-app/src/content/TextField.jsx
+++ my-app/src/content/TextField.jsx
@@ -1,40 +1,21 @@
 import React from 'react';
-function TextField(props){
-    const [val,setVal]=React.useState({err:true,errText:"Should Not Be Empty",val:""});
-    var err,errText;
+function TextField(props) {
+  console.log(props.err);
+  const [val, setVal] = React.useState("");
 
-    function validate(txt){
-      var pattern=/^[a-zA-Z ]*$/;
-      return pattern.test(txt);
-    }
-
-    function changeText(event){
-      var inputVal=event.target.value.trimLeft().replace(/\s\s+/g, ' ');
-      if(inputVal!==""){
-          err=false;
-          errText="";
-          if(inputVal.length>30){
-            err=true;
-            errText="Length Should not be greater than 30";
-          } else {
-            if(!validate(inputVal)){
-              err=true;
-              errText="Contains only text";
-            }
-          }
-      } else {
-        err=true;
-        errText="Should Not Be Empty";
-      }
-        setVal({err:err,errText:errText,val:inputVal});
-    }
-    const a="textField "+props.class+" form-control";
-    return(
-      <span className="form-group">
-          <input id={props.id} type="text" value={val.val} onChange={changeText} className={a} autoComplete="off" />
-          <small className="form-text" style={{"color":"red"}}>{val.err?val.errText:""}</small>
-      </span>
-    );
+  function changeText(event) {
+    var inputVal = event.target.value.trimLeft().replace(/\s\s+/g, ' ');
+    // if (props.setFn) // if the function is set
+    //   props.setFn(inputVal);
+    setVal(inputVal);
+  }
+  const a = "textField " + props.class + " form-control";
+  return (
+    <span className="form-group">
+      <input id={props.id} type="text" value={val} onChange={changeText} className={a} autoComplete="off" />
+      <small className="form-text" style={{ "color": "red" }}>{props.err}</small>
+    </span>
+  );
 }
 
 export default TextField;
